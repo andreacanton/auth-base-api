1f4af1ca4a62519fd698e6b5d55acdd3cb9b7a15
Andrea Canton <andrea.canton@gmail.com>
2021-12-25 14:21:43 +0100

    install helmet

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const app = express();
 
 app.use(cors());
+
+app.use(
+  helmet({
+    dnsPrefetchControl: false,
+    frameguard: false,
+    ieNoOpen: false,
+  }),
+);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
